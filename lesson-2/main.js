c2d241634b248d611864541112b365805aee55fb
Nikita-front56 <cs_golds@bk.ru>
2025-04-05 11:22:01 +0500

    Add files via upload

---
+++ lesson-2/main.js
@@ -0,0 +1,102 @@
+// МАССИВЫ
+
+// const numbers = [1,'2',true,[1,2,3],5]
+
+// console.log(numbers)
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers[4] = 6
+
+// console.log(numbers)
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log (numbers[i] + 1)
+// }
+
+// ФУНКЦИИ
+
+// function sumNumbers() {
+//     return 5 + 5
+// }
+
+// const result = sumNumbers()
+// console.log(result)
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// function helloName(firstNumber, secondNumber) {
+//     return firstNumber + secondNumber
+// }
+
+// const result = helloName(3, 10)
+// console.log(result)
+// console.log(helloName(-2, 2))
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// const users = ['Jonh', 'Ann', 'Alex', 'Max']
+
+// function checkForCopyItem(array, item) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === item) {
+//             return `There is a copy of the ${item} in array`
+//         }
+//     }
+//     return `There is no such item in the array`
+// }
+
+// console.log(checkForCopyItem(users, 'Alex'))
+// const users = ['Jonh', 'Ann', 'Alex', 'Max']
+
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// ДЗ
+1
+
+function helloName(name) {
+    return ('Hello, ' + name + '!')
+}
+
+console.log (helloName("Max"))
+
+// +++++++++++++++++++++++++++++++++++++++++++
+// 2
+
+const numbers = [ 1, 3, 5, 7, 12, 17, 11, 15];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 10) {
+        console.log(numbers[i]);
+    }
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++
+// 3
+
+// function(2, 3, 'minus')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
